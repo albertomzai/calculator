fbf0e5f60157b0965b7a99e124826616af84d965
Alberto Martínez <albertomz@gmail.com>
2025-08-12 01:51:23 +0200

    Agente [BACKEND]: Completa la construcción de la etapa

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-# Inicializar la aplicación Flask
-app = Flask(__name__)
-# Configuración mínima (puede incluir SECRET_KEY si se desea)
-app.config['JSON_SORT_KEYS'] = False
-
-# Importar las rutas para que se registren en la app
-from . import routes

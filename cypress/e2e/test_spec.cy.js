9c3cd13e0f02e81fb8b819cdd3f0f8384b532285
Alberto Martínez <albertomz@gmail.com>
2025-08-15 02:16:59 +0200

    Agente [E2E-Tester]: Genera ficheros de prueba

--- cypress/e2e/test_spec.cy.js
+++ cypress/e2e/test_spec.cy.js
@@ -1,20 +1,19 @@
 describe('Prueba E2E de la Calculadora', () => {
   it('debe calcular 7+3 y mostrar 10', () => {
+    // Visitar la página principal
     cy.visit('/');
-    
-    // Ingresar 7
-    cy.get('[data-testid="num-7"]').click();
-    
+
+    // Ingresar el número 7
+    cy.get('[data-testid="button-7"]').click();
+
     // Operador +
-    cy.get('[data-testid="op-add"]').click();
-    
-    // Ingresar 3
-    cy.get('[data-testid="num-3"]').click();
-    
+    cy.get('[data-testid="button-plus"]').click();
+
+    // Ingresar el número 3
+    cy.get('[data-testid="button-3"]').click();
+
     // Igual y verificar resultado
-    cy.get('[data-testid="equal"]').click();
-    
-    // Esperamos que la pantalla muestre el resultado 10
+    cy.get('[data-testid="button-equal"]').click();
     cy.get('[data-testid="display"]').should('contain.text', '10');
   });
 });
